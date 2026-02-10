269a41f3f619f34c53e6af0f338e3a3279fd7e5c
EricPuchalski <ericpuchalski12@gmail.com>
2026-02-10 19:45:54 -0300

    init

---
+++ src/main/java/com/infotienda/InfotiendaApplication.java
@@ -0,0 +1,13 @@
+package com.infotienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfotiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfotiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infotienda/InfotiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfotiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
